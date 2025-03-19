dfbe6b630f069655e162efbb53f7c0dadf808d20
RaghadAlmehmadi <raghadalharbi078@gmail.comraghadalharbi078@gmail.com>
2025-03-19 12:50:56 +0300

    Updated EnrollTrainingProgramUseCase

--- app/src/main/java/com/example/training_enrollment/authentication/domain/useCases/EnrollTrainingProgramUseCase.kt
+++ app/src/main/java/com/example/training_enrollment/authentication/domain/useCases/EnrollTrainingProgramUseCase.kt
@@ -3,7 +3,7 @@ package com.example.training_enrollment.authentication.domain.useCases
 
 
 class EnrollTrainingProgramUseCase {
-    suspend fun execute(name: String, email: String, programName: String): String {
+     fun execute(name: String, email: String, programName: String): String {
         if (name.isBlank() || email.isBlank()) {
             throw IllegalArgumentException("Name and Email cannot be empty.")
         }
